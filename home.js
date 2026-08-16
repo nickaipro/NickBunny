47aa240b3309db707259c2834915f5aeb363c93b
nickaipro <nickai.pro1@gmail.com>
2026-08-15 20:19:45 -0500

    Final version V1

--- home.js
+++ home.js
@@ -1,5 +1,53 @@
+//Declaration of variables
+
+
 const songInput = document.getElementById("songInput")
 const formSearch = document.getElementById("formSearch")
+const songContainer = document.getElementById("songContainer")
+const songCover = document.getElementById("songCover")
+const songTitle = document.getElementById("songTitle")
+const songArtist = document.getElementById("songArtist")
+const songMusic = document.getElementById("songMusic")
+const playSong = document.getElementById("playSong")
+const pauseSong = document.getElementById("pauseSong")
+
+
+//Async function for searching the song data
+
+async function searchedSongs(searchSong){
+    const response = await fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(searchSong)}&media=music&entity=song&limit=1`)
+    const data = await response.json()
+    console.log(data.results)
+    const song = data.results[0]
+
+    //Display the results
+
+    songContainer.style.display = "flex"
+    songTitle.textContent = song.trackName
+    songCover.src = song.artworkUrl100
+    songArtist.textContent = `Artist/s:  ${song.artistName}`
+    
+    songMusic.src = song.previewUrl
+    
+    playSong.addEventListener("click",()=>{
+        songMusic.play()
+        
+        
+    })
+
+    pauseSong.addEventListener("click",()=>{
+        songMusic.pause()
+    })
+    
+
+
+
+
+
+
+}
+
+//function for receive the user´s input and make to call the song search engine
 
 formSearch.addEventListener("submit", (event)=>{
     event.preventDefault()
@@ -7,7 +55,9 @@ formSearch.addEventListener("submit", (event)=>{
     const searchSong = songInput.value
 
     
-    console.log(searchSong)
+    searchedSongs(searchSong)
 
     songInput.value = ""
 })
+
+
